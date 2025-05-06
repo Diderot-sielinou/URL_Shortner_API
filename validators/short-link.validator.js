7223ffbb6a2a7f084f479333999daf9f23e45033
diderot-sielinou <diderotsielinou@gmail.com>
2025-05-06 18:18:25 +0100

    modification of the regular expression of the original URL verification

--- validators/short-link.validator.js
+++ validators/short-link.validator.js
@@ -1,33 +1,36 @@
 import Joi from "joi";
 
 const linkSchema = Joi.object({
-  shortCode:  Joi.string()
-  .alphanum()                // Letters and numbers only
-  .min(4)                    // Minimum length (adjust as needed)
-  .max(10)                   // Maximum length (adjust as needed)
-  .optional()
-  .messages({
-    'string.base': 'Shortcode must be a string',
-    'string.alphanum': 'Shortcode can only contain letters and numbers',
-    'string.min': 'Shortcode must be at 4 least  characters long',
-    'string.max': 'Shortcode must be at 10 most  characters long',
-  }),
+  shortCode: Joi.string()
+    .alphanum() // Letters and numbers only
+    .min(4) // Minimum length (adjust as needed)
+    .max(10) // Maximum length (adjust as needed)
+    .optional()
+    .messages({
+      "string.base": "Shortcode must be a string",
+      "string.alphanum": "Shortcode can only contain letters and numbers",
+      "string.min": "Shortcode must be at 4 least  characters long",
+      "string.max": "Shortcode must be at 10 most  characters long",
+    }),
   originalUrl: Joi.string()
-    .pattern(/^https?:\/\/(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\/[^\s]*)?$/)
+    .pattern(
+      /^https?:\/\/(?:[a-zA-Z0-9-]+\.)+(?:[a-zA-Z]{2,})(?::\d+)?(?:\/[^\s]*)?$/
+    )
     .required()
     .messages({
       "string.base": "L'URL doit être une chaîne de caractères",
       "string.pattern": "L'URL fournie n'est pas valide",
       "any.required": "L'URL est requise",
     }),
-    expiresAtString: Joi.string()
+  expiresAtString: Joi.string()
     .pattern(
       /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}( ([01][0-9]|2[0-3]):[0-5][0-9])?$/
     )
     .messages({
-      "string.pattern.base": 'Invalid format. Use "DD/MM/YYYY" or "DD/MM/YYYY HH:MM"',
+      "string.pattern.base":
+        'Invalid format. Use "DD/MM/YYYY" or "DD/MM/YYYY HH:MM"',
     })
-    .optional()
+    .optional(),
 });
 
 export function createShortLinkValidator(req, res, next) {
